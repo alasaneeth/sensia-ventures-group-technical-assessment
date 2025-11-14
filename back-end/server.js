643d58a932487b6349df601990ed983848e0ef18
alasaneeth <alasaneeth22@gmail.com>
2025-11-14 20:19:17 +0400

    chore: Add database Configration

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-const app = express();
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error connecting to database', err.stack);
+  }
+  console.log('Database connected successfully!');
+  release(); 
+  pool.end(); 
 });
